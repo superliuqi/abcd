0f8419ce4bd973655ca50c1ed6ce2a5bf21af315
liuqi <359380474@qq.com>
2018-08-24 09:29:50 +0800

    sf

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -8,7 +8,6 @@ class GitController extends Controller
 {
     public function pull()
     {
-        file_put_contents('./hello.txt','hello',FILE_APPEND);
         exec('cd /data/wwwroot/default/abcd && git pull');
     }
 }
